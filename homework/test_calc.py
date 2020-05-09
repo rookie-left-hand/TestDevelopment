e7deedf124786486818b36dcb74b2fcf367d25e5
luan <luan199433@gmail.com>
2020-05-09 22:36:40 +0800

    pytest作业，最终版本

--- homework/test_calc.py
+++ homework/test_calc.py
@@ -7,19 +7,19 @@ import yaml
 from python_code.calc import Calc
 import pytest
 
+
 class TestCalc:
     def setup(self):
         self.calc = Calc()
 
-
-    @pytest.mark.parametrize(["a","b","c"],yaml.safe_load(open(os.path.dirname(__file__)+"\\test_add_data.yaml")))
-    def test_add(self,a,b,c):
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open(os.path.dirname(__file__) + "\\test_add_data.yaml")))
+    def test_add(self, a, b, c):
         print(f"测试数据分别是：{a},{b}")
-        result = self.calc.add(a,b)
+        result = self.calc.add(a, b)
         print(result)
         assert c == result
 
-    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open(os.path.dirname(__file__)+"\\test_div_data.yaml")))
+    @pytest.mark.parametrize(["a", "b", "c"], yaml.safe_load(open(os.path.dirname(__file__) + "\\test_div_data.yaml")))
     def test_div_01(self, a, b, c):
         """正常情况下测试"""
         print(f"测试数据分别是：{a},{b}")
@@ -27,13 +27,20 @@ class TestCalc:
         print(result)
         assert c == result
 
-    @pytest.mark.parametrize(["a","b"],["5","0"])
-    def test_div_02(self,a,b):
+    @pytest.mark.parametrize(["a", "b"], [(5, 0)])
+    def test_div_02(self, a, b):
         """除数为零"""
-        print(f"测试数据分别是：{a},{b}")
-        # with pytest.raises(ZeroDivisionError):
+        print(f"测试数据分别是：{a}, {b}")
+        with pytest.raises(ZeroDivisionError):
+            self.calc.div(5, 0)
+        # print(self.calc.div(5, 0))
+
+    @pytest.mark.parametrize(["a", "b"], [(10, 3)])
+    def test_div_03(self, a, b):
+        """商为无限循环小数时"""
+        print(f"测试数据分别是：{a}, {b}")
         result = self.calc.div(a, b)
-        assert result == 5
+        assert 3.333333333 == round(result, 9)
 
 
 if __name__ == '__main__':
